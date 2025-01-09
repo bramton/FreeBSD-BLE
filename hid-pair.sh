f8ca20f2c032a7c53eaad15b3e2c3cb808335094
Robin Haberkorn <robin.haberkorn@googlemail.com>
2025-01-09 21:47:45 +0300

    added a HID pairing script: allows pairing and activation of a BLE device by name in a single command
    * for instance: doas ./hid-pair.sh ELECOM TrackBall
    * This means you have to re-pair every time.
    * The database is also removed every time.
    * Should work both with random and static addresses.
    * I don't think we can sensibly separate the pairing phase and keep lehid running
      in the background, or in other words:
      It is not worth working on the current codebase to allow that.

---
+++ hid-pair.sh
@@ -0,0 +1,30 @@
+#!/usr/local/bin/bash
+set -e
+
+NAME="$*"
+
+hccontrol le_enable enable
+# May already be enabled
+hccontrol le_set_scan_enable enable || true
+sleep 1
+
+ADDR=($(hccontrol read_neighbor_cache | \
+        awk -vname="$NAME" '/[RP] / {type = $1; addr = $2} tolower($0)~("name: " tolower(name)) {print type, addr}'))
+if [ "${ADDR[0]}" = R ]; then
+	BTADDR=-r
+fi
+BTADDR="$BTADDR ${ADDR[1]}"
+echo Connecting to Bluetooth address ${ADDR[1]}...
+
+hccontrol le_set_scan_enable disable
+
+lepair/lepair $BTADDR >lesecd/hcsecd.conf
+
+(cd lesecd; lesecd) &
+LESECD_PID=$!
+trap "kill $LESECD_PID" EXIT HUP TERM INT
+
+sleep 1
+
+rm -f hoge.db
+nice -n -5 lehid/lehid -s $BTADDR
